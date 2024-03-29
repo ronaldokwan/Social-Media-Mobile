d674b32f7a459b5ef2ef2e2e3fe041f85001970b
Ronaldo Kwan <ronaldokwan17@gmail.com>
2024-03-29 22:17:13 +0700

    Add "Don't have an account?" text

--- react-native/screens/Login.js
+++ react-native/screens/Login.js
@@ -58,6 +58,7 @@ export default function Login({ navigation }) {
         disabled={loading}
         loading={loading}
       /> */}
+      <Text>Don't have an account?</Text>
       <Button
         title="Register"
         onPress={() => {
